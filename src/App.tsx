026819d0cd0956601160ca1d324b14094203e801
mariessad <74148632+mariessad@users.noreply.github.com>
2023-05-31 14:21:20 -0400

    add card component, add img folder, add type props to component, pass component to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import QuestionCard from './components/QuestionCard';
 
 
 function App() {
+
+  const startQuiz = async () => {
+
+  }
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  const nextQuestion = () => {
+
+  }
+
+
   return (
     <div className="App">
       <h1>Quiz</h1>
+      <button className='start-button' onClick={startQuiz}/>
+      <p className='score'>Score:</p>
+      <p>Loading Questions ...</p>
+      <QuestionCard/>
+      <button className='next-button' onClick={nextQuestion}>Next</button>
     </div>
   );
 }
