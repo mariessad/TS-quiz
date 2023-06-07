cf87ccc679645fc1c7a8b168edfd29379ea4c029
mariessad <74148632+mariessad@users.noreply.github.com>
2023-06-07 18:27:20 -0400

    more css

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -46,10 +46,25 @@ h1{
     background-clip: text;
     // -moz-background-clip: text;
     // -moz-text-fill-color: transparent;
-    filter: drop-shadow(2px 2px #0085a);
+    filter: drop-shadow(2px 2px #0085a3);
     font-size: 70px;
     text-align: center;
     margin: 20px;
     font-weight: 400;
+}
+
+.start, .next {
+    cursor: pointer;
+    background: linear-gradient(180deg, #fff, #ffcc91);
+    border: 2px solid #d38558;
+    box-shadow: 0px 5px 10px rgba(0, 0,0 0.25);
+    border-radius: 10px;
+    height: 40px;
+    margin: 20px 0;
+    padding: 0 40px;
+}
+
+.start {
+    max-width: 200px;
 }
 `
